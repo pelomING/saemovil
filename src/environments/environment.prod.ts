2006db2742aabd28b11f5e5bb0acd9d1d02cce8c
Patracom <pelomingenieria2024@gmail.com>
2023-11-07 21:11:15 -0300

    cambio aplicado 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url1: 'https://backend-saemovil-production.up.railway.app',
   url: 'https://backend-saemovil-development.up.railway.app',
-  version:'SAE Móvil V1.1.1'
+  version:'SAE Móvil V1.1.2'
 };
