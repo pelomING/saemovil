caf732162e8836fd7773a0c2c31cc9de64c31c84
Patracom <pelomingenieria2024@gmail.com>
2023-12-18 11:35:45 -0300

    prueba actualizar service worket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //url: 'https://backend-saemovil-production.up.railway.app',
   url: 'https://backend-saemovil-development.up.railway.app',
-  version:'SAE Móvil V1.1.4'
+  version:'SAE Móvil V1.1.5'
 };
